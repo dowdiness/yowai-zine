6f01c360543598848d2245aeb57c871aac2fab37
Koji Ishimoto <koji.ishimoto@gmail.com>
2020-11-03 17:10:48 +0900

    add useTategaki hook

---
+++ src/hooks/useTategaki.ts
@@ -0,0 +1,56 @@
+import { useRef, useEffect, MutableRefObject } from 'react'
+import { isFirefox } from './util'
+import { debounce } from 'lodash'
+
+export interface Tategaki {
+  tategakiRef: MutableRefObject<HTMLElement>
+}
+
+const useTategaki = (): Tategaki => {
+  const tategakiRef = useRef<HTMLElement>(null!)
+
+  const adjustSize = () => {
+    // bug fix
+    if (isFirefox) {
+      tategakiRef.current.style.height = 'auto'
+
+      requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+          if (!tategakiRef) {
+            return
+          }
+
+          tategakiRef.current.style.height = ''
+          tategakiRef.current.style.paddingBottom = ''
+          const fullHeight = tategakiRef.current.scrollHeight
+          const defaultHeight = Number(getComputedStyle(tategakiRef.current).height.replace('px', ''))
+          tategakiRef.current.style.paddingBottom = `${fullHeight - defaultHeight}px`
+        })
+      })
+      return
+    }
+
+    tategakiRef.current.style.paddingBottom = ''
+    const fullHeight = tategakiRef.current.scrollHeight
+    const defaultHeight = Number(getComputedStyle(tategakiRef.current).height.replace('px', ''))
+    console.log(fullHeight, defaultHeight)
+    tategakiRef.current.style.paddingBottom = `${fullHeight - defaultHeight}px`
+  }
+
+  const debouncedAdjustSize = debounce(adjustSize, 100)
+
+  useEffect(() => {
+    adjustSize()
+    window.addEventListener('load', adjustSize)
+    window.addEventListener('resize', debouncedAdjustSize)
+    return () => {
+      debouncedAdjustSize.cancel()
+      window.removeEventListener('load', adjustSize)
+      window.removeEventListener('resize', debouncedAdjustSize)
+    }
+  }, [tategakiRef])
+
+  return { tategakiRef }
+}
+
+export default useTategaki
--- src/hooks/util.ts
+++ src/hooks/util.ts
@@ -2,6 +2,8 @@ import isDeepEqualReact from 'fast-deep-equal/react';
 
 export const isClient = typeof window === 'object';
 
+export const isFirefox = isClient && navigator.userAgent.includes('Firefox')
+
 export const on = (obj: any, ...args: any[]) => obj.addEventListener(...args);
 
 export const off = (obj: any, ...args: any[]) => obj.removeEventListener(...args);
