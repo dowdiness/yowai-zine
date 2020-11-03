db6b3255c751b8e7871bf22eb2f278cd628b7266
Koji Ishimoto <koji.ishimoto@gmail.com>
2020-11-03 18:30:03 +0900

    improve footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,27 +1,29 @@
+import { Link } from "gatsby"
+import Image from "src/components/Image"
+
 export type FooterProps = {}
 
 export const Footer: React.FCX<FooterProps> = ({ className }) => {
   return (
-    <footer className={className}>
-      <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
-        <nav className="flex flex-wrap justify-center -mx-5 -my-2">
+    <footer className={`${className}`}>
+      <div className="flex flex-col items-center max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
+        <Link to="/">
+          <Image
+            filename="yowai-logo.png"
+            alt="yowai-logo"
+            className="w-32 h-32 text-center transition-opacity duration-300 ease-out hover:opacity-50"
+          />
+        </Link>
+        {/* <nav className="flex flex-wrap justify-center -mx-5 -my-2">
           <div className="px-5 py-2">
             <a href="#" className="text-base leading-6 text-gray-500 hover:text-gray-900">
               About
             </a>
           </div>
-        </nav>
-        {/* <div className="flex justify-center mt-8 space-x-6">
-          <a href="#" className="text-gray-400 hover:text-gray-500">
-            <span className="sr-only">Facebook</span>
-            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
-              <path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" />
-            </svg>
-          </a>
-        </div> */}
-        <p className="mt-8 text-base italic leading-6 text-center text-gray-400">
+        </nav> */}
+        <h4 className="mt-8 text-base italic leading-6 text-center text-gray-400">
           Don't kill. Protect your weakness.
-        </p>
+        </h4>
       </div>
     </footer>
   )
