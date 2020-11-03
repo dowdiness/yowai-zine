51be1b6bc69e7154c56a02cecb307b7e2246dd3d
Koji Ishimoto <koji.ishimoto@gmail.com>
2020-11-03 20:16:07 +0900

    add gatsby-remark-check-links

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,7 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          `gatsby-remark-check-links`,
           {
             resolve: `gatsby-remark-images`,
             options: {
