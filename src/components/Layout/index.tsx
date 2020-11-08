2c02512f9ba577d3f3848dafb3d6b9aac17545d0
Koji Ishimoto <koji.ishimoto@gmail.com>
2020-11-08 19:53:19 +0900

    add react-div-100vh for loading animation

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import Header from './Header'
 import Footer from './Footer'
+import Div100vh from 'react-div-100vh'
 
 import { motion, AnimatePresence } from 'framer-motion'
 
@@ -9,7 +10,7 @@ type LayoutProps = {
   location?: Location
 }
 
-const transition = { duration: 2, ease: [0.43, 0.13, 0.23, 0.96] }
+const transition = { duration: 1.6, ease: [0.43, 0.13, 0.23, 0.96] }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   // const rootPath = `${__PATH_PREFIX__}/`
@@ -32,44 +33,46 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   useEffect(() => {
     setTimeout(() => {
       setFinishLoading(false)
-    }, 5000)
+    }, 2400)
   }, [])
 
   return (
     <AnimatePresence exitBeforeEnter>
       {finishLoading ? (
-        <motion.section
-          key="loading"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={transition}
-          className="fixed flex flex-col items-center justify-center w-screen h-screen text-5xl"
-        >
-          <motion.img
+        <Div100vh>
+          <motion.section
+            key="loading"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={transition}
-            src={data.allFile.nodes[0].publicURL}
-            alt={data.allFile.nodes[0].name}
-            className="w-64 h-64 mb-16"
-          />
-          <div className="flex flex-col items-center mb-6 space-y-3 font-sans">
-            <h3 className="text-4xl italic font-extrabold text-yowai">
-              Don't kill.
-            </h3>
-            <h3 className="text-4xl italic font-extrabold text-yowai">
-              Protect your
-            </h3>
-            <h3 className="text-4xl italic font-extrabold text-yowai">
-              weakness.
-            </h3>
-          </div>
-          <h1 className="flex justify-center text-6xl italic font-black text-white neumorphism">
-            弱いzine
-          </h1>
-        </motion.section>
+            className="fixed flex flex-col items-center justify-center w-screen h-screen text-5xl"
+          >
+            <motion.img
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={transition}
+              src={data.allFile.nodes[0].publicURL}
+              alt={data.allFile.nodes[0].name}
+              className="w-64 h-64 mb-4 -mt-12"
+            />
+            <div className="flex flex-col items-center mb-6 space-y-2 font-sans">
+              <h3 className="text-4xl italic font-extrabold text-yowai">
+                Don't kill.
+              </h3>
+              <h3 className="text-4xl italic font-extrabold text-yowai">
+                Protect your
+              </h3>
+              <h3 className="text-4xl italic font-extrabold text-yowai">
+                weakness.
+              </h3>
+            </div>
+            <h1 className="flex justify-center text-6xl italic font-black text-white neumorphism">
+              弱いzine
+            </h1>
+          </motion.section>
+        </Div100vh>
       ) : (
         <motion.div
           key="loading"
