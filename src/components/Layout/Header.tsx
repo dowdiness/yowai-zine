533e9fa36137da2313527fcfa6ae3695559bef0f
Koji Ishimoto <koji.ishimoto@gmail.com>
2020-11-21 15:55:18 +0900

    hide header when user enter website or scroll down

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'gatsby'
 
 // hooks
 import useWindowScroll from 'src/hooks/useWindowScroll'
+import usePrevious from 'src/hooks/usePrevious'
 
 export type HeaderProps = unknown
 export const Header: React.FCX<HeaderProps> = ({ className }) => {
-  // const { y } = useWindowScroll()
+  const { y } = useWindowScroll()
+  const previousY = usePrevious(y)
+  const [inView, setInView] = useState(false)
+
+  useEffect(() => {
+    if(previousY) y < previousY ? setInView(true) : setInView(false)
+  }, [y])
 
   return (
     <header
-      className={`fixed top-0 flex items-center justify-center w-full h-16 ${className}`}
+      className={`fixed top-0 flex items-center justify-center w-full h-16 transform transition-transform duration-150 ${inView ? '' : '-translate-y-16' } ${className}`}
     >
       <Link to="/">
         <h1 className="flex justify-center text-4xl italic font-black text-white neumorphism">
