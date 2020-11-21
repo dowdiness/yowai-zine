ff375dddc54cfe5efcabb8e785b104edb6917073
Koji Ishimoto <koji.ishimoto@gmail.com>
2020-11-21 11:57:51 +0900

    add introduction in index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,20 @@ import React from 'react'
 import { graphql, PageProps, Link } from 'gatsby'
 
 import { GatsbySeo } from 'gatsby-plugin-next-seo'
+import useCircularText from 'src/hooks/useCircularText'
+import useWindowSize from 'src/hooks/useWindowSize'
 
 const BlogIndex: React.FC<PageProps<GatsbyTypes.IndexPageQuery>> = ({
   data,
 }) => {
   const posts = data.allMarkdownRemark.nodes
   const home = data.home
+  const { width } = useWindowSize()
+  const circle = home?.catchphrase
+  const ratio = width > 550 ? width * 0.003 : width * 0.004
+  const catchphrase = circle?.repeat(Math.ceil(ratio)).slice(0, circle?.length * ratio)
+
+  const { circleTextRef } = useCircularText<HTMLHeadingElement>(width > 550 ? width * 0.2 : width * 0.35, catchphrase!)
 
   return (
     <>
@@ -16,9 +24,21 @@ const BlogIndex: React.FC<PageProps<GatsbyTypes.IndexPageQuery>> = ({
         <Link to="/vol/0/">
           <img src={home?.image} width="364" height="514" />
         </Link>
-        <p className="text-lg leading-loose tracking-widest text-center whitespace-pre-wrap md:xl-text">
-          { home?.introduction }
-        </p>
+        <section className="flex items-center justify-center w-full h-auto max-w-screen-lg mx-auto sm:h-screen">
+        <div>
+          <div className="relative mb-12 sm:float-left shape-outside sm:mb-0">
+            <h3 className="mx-auto text-xxs animate-spin-slow" ref={circleTextRef} />
+            <h2 className="absolute w-full h-12 font-sans text-3xl text-center transform -translate-x-1/2 -translate-y-1/2 md:text-4xl xl:text-6xl inset-1/2">げんきですか？</h2>
+          </div>
+          <div className="text-center sm:text-left">
+            <p
+              className="inline-block text-base tracking-widest text-justify whitespace-pre-wrap sm:contents text-character h-160 column-count-2 column-gap-12 vertical-rl sm:text-xl sm:text-left sm:column-count-none sm:horizontal-tb sm:h-full leading-loosest"
+            >
+              {home?.introduction}
+            </p>
+          </div>
+        </div>
+      </section>
       </div>
       <ol style={{ listStyle: 'none' }}>
         {posts.map(post => {
@@ -84,6 +104,7 @@ export const pageQuery = graphql`
     }
     home: pagesJson(path: {eq: "/"}) {
       image
+      catchphrase
       introduction
     }
   }
