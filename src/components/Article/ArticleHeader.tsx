86a89a1677b5f3552d91b39d27b22a01550b81fd
Koji Ishimoto <koji.ishimoto@gmail.com>
2020-12-14 16:35:48 +0900

    adjust font sizes in headings

--- src/components/Article/ArticleHeader.tsx
+++ src/components/Article/ArticleHeader.tsx
@@ -7,11 +7,11 @@ export type ArticleHeaderProps = {
 
 const ArticleHeader: React.FCX<ArticleHeaderProps> = ({ className, title, author }) => {
   return (
-    <header className={`flex flex-col items-center space-y-12 my-24 ${className}`}>
-      <h1 className="text-3xl font-extrabold text-left text-gray-900 font-selif sm:text-4xl md:text-5xl lg:text-6xl" itemProp="headline">
+    <header className={`flex flex-col items-center space-y-6 md:space-y-8 mt-12 mb-16 ${className}`}>
+      <h1 className="font-sans text-3xl font-extrabold text-left text-gray-900 sm:text-4xl md:text-5xl lg:text-6xl" itemProp="headline">
         {title}
       </h1>
-      <h2 className="font-sans text-2xl italic font-medium text-center text-gray-800 sm:text-3xl md:text-4xl lg:text-5xl">
+      <h2 className="font-sans text-2xl font-medium text-center text-gray-600 sm:text-3xl md:text-4xl lg:text-5xl">
         {author}
       </h2>
     </header>
--- src/components/Article/ArticleSideHeader.tsx
+++ src/components/Article/ArticleSideHeader.tsx
@@ -8,10 +8,10 @@ export type ArticleSideHeaderProps = {
 const ArticleSideHeader: React.FCX<ArticleSideHeaderProps> = ({ className, title, author }) => {
   return (
     <header className={`w-full flex items-center space-y-4 vertical-rl ${className}`}>
-      <h1 className="text-xl font-extrabold text-center text-gray-900 font-selif sm:text-2xl md:text-3xl" itemProp="headline">
+      <h1 className="font-sans text-xl font-extrabold text-center text-gray-900 sm:text-2xl md:text-3xl" itemProp="headline">
         {title}
       </h1>
-      <h2 className="font-sans text-lg italic font-medium text-center text-gray-800 sm:text-xl md:text-2xl">
+      <h2 className="font-sans text-lg font-medium text-center text-gray-800 sm:text-xl md:text-2xl">
         {author}
       </h2>
     </header>
