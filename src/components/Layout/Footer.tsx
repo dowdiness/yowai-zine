09d9de597d6e8365477f0b23c5c33501816628d2
Koji Ishimoto <koji.ishimoto@gmail.com>
2020-12-16 04:10:17 +0900

    dont use gatsby imgae in footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import { StaticImage } from "gatsby-plugin-image"
+// @ts-ignore
+import logo from "./yowai-logo.png"
 
 export type FooterProps = unknown
 
@@ -10,21 +11,12 @@ export const Footer: React.FCX<FooterProps> = ({ className }) => {
     <footer className={`${className}`}>
       <div className="flex flex-col items-center space-y-8 overflow-hidden">
         <Link className="" to="/">
-          <StaticImage
-            width={128}
-            height={128}
-            src="yowai-logo.png"
+          <img
+            src={logo}
             alt="yowai-logo"
             className="w-32 h-32 text-center transition-opacity duration-300 ease-out hover:opacity-50"
           />
         </Link>
-        {/* <nav className="flex flex-wrap justify-center -mx-5 -my-2">
-          <div className="px-5 py-2">
-            <a href="#" className="text-base leading-6 text-gray-500 hover:text-gray-900">
-              About
-            </a>
-          </div>
-        </nav> */}
         <h4 className="mt-8 text-base italic leading-6 text-center text-gray-600">
           Don't kill. Protect your weakness.
         </h4>
