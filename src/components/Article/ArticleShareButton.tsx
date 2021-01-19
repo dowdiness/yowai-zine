31678695477c6a33475e88cb7f174abdfe92d8cc
Koji Ishimoto <koji.ishimoto@gmail.com>
2021-01-20 01:27:54 +0900

    resolve error "navigator" is not available during server side rendering.

--- src/components/Article/ArticleShareButton.tsx
+++ src/components/Article/ArticleShareButton.tsx
@@ -15,39 +15,6 @@ export type ArticleShareButtonProps = {
   articleDescription: string
 }
 
-type ShareApiProps = {
-  title: string
-  text: string
-  url: string
-  size: number
-}
-
-const ShareApi: React.FCX<ShareApiProps> = ({ className, title, text, url, size }) => {
-  const shareData: Required<ShareData> = {
-    title: title,
-    text: text,
-    url: url,
-  }
-
-  const share = async () => {
-    await navigator.share(shareData).catch(error => {
-      console.error(error.message)
-    })
-  }
-
-  if (!!navigator.share) {
-    return (
-      <div className={className}>
-        <button aria-label="share" className="react-share__ShareButton">
-          <BiShareAlt onClick={share} size={size} />
-        </button>
-      </div>
-    )
-  }
-
-  return null
-}
-
 const ArticleShareButton: React.FCX<ArticleShareButtonProps> = ({ className, articleTitle, articleUrl, articleDescription }) => {
   const [iconSize, setIconSize] = useState(48)
   const changeIconSize = (e: MediaQueryListEvent) => {
@@ -72,6 +39,18 @@ const ArticleShareButton: React.FCX<ArticleShareButtonProps> = ({ className, art
     }
   }, [])
 
+  const shareData: Required<ShareData> = {
+    title: articleTitle,
+    text: articleDescription,
+    url: articleUrl,
+  }
+
+  const share = async () => {
+    await navigator.share(shareData).catch(error => {
+      console.error(error.message)
+    })
+  }
+
   return (
     <div className={`flex flex-col items-center space-y-8 sm:space-y-12 md:space-y-16 xl:space-y-20 ${className}`}>
       <h3 className="font-sans text-xl font-black uppercase sm:text-2xl md:text-3xl xl:text-4xl">この記事をシェアする</h3>
@@ -91,13 +70,13 @@ const ArticleShareButton: React.FCX<ArticleShareButtonProps> = ({ className, art
             <LineIcon round size={iconSize} />
           </LineShareButton>
         </div>
-        <ShareApi
-          className="w-12 h-12 lg:w-18 lg:h-18"
-          url={articleUrl}
-          title={articleTitle}
-          text={articleDescription}
-          size={iconSize}
-        />
+        {typeof navigator === 'object' && navigator.share && (
+          <div className="w-12 h-12 lg:w-18 lg:h-18">
+            <button aria-label="share" className="react-share__ShareButton">
+              <BiShareAlt onClick={share} size={iconSize} />
+            </button>
+          </div>
+        )}
       </div>
     </div>
   )
