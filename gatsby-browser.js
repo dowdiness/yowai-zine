e8c876488b8d11f17855f99f2b8bb015f15c91b7
Koji Ishimoto <koji.ishimoto@gmail.com>
2021-03-17 20:43:25 +0900

    disable web font for perfomance

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,16 @@
 // custom fontsource
-import "fontsource-noto-serif-jp"
-import "fontsource-noto-sans-jp"
-import "fontsource-jost"
+// import "fontsource-noto-serif-jp/400.css"
+// import "fontsource-noto-serif-jp/500.css"
+// import "fontsource-noto-serif-jp/700.css"
+// import "fontsource-noto-serif-jp/900.css"
+// import "fontsource-noto-sans-jp/400.css"
+// import "fontsource-noto-sans-jp/500.css"
+// import "fontsource-noto-sans-jp/700.css"
+// import "fontsource-noto-sans-jp/900.css"
+// import "fontsource-jost/400.css"
+// import "fontsource-jost/500.css"
+// import "fontsource-jost/700.css"
+// import "fontsource-jost/900.css"
 
 // custom CSS styles
 // Separate up tailwindcss files for fast recompile https://github.com/brianlovin/brian-lovin-next/pull/1193
