853048c541776c76f6f936405693a4ad67295d3a
Koji Ishimoto <koji.ishimoto@gmail.com>
2021-03-24 22:21:22 +0900

    resolve AudioProvider ssr problem

--- src/components/AudioPlayer/AudioProvider.tsx
+++ src/components/AudioPlayer/AudioProvider.tsx
@@ -1,11 +1,29 @@
-import React, { useRef, ReactNode } from 'react'
+import React, { useRef, useEffect, ReactNode } from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 
 import { createContext } from 'use-context-selector'
 
 export const AudioContext = createContext<React.MutableRefObject<HTMLAudioElement>|null>(null!)
 
 export const AudioProvider = ({ children }: { children: ReactNode }) => {
-  const audioRef = useRef<HTMLAudioElement>(new Audio('001.wav'))
+  const { audio } = useStaticQuery(
+    graphql`
+      query {
+        audio: allFile(filter: {id: {eq: "e8d9270b-16a0-5bdb-b214-c2aff1bb7160"}}) {
+          edges {
+            node {
+              publicURL
+            }
+          }
+        }
+      }
+    `
+  )
+
+  let audioRef: React.MutableRefObject<HTMLAudioElement> | null = null
+  useEffect(() => {
+    audioRef = useRef<HTMLAudioElement>(new Audio(audio.edges[0].node.publicURL))
+  }, [])
 
   return (
     <AudioContext.Provider value={audioRef}>
