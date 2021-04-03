f1be36c9191f8b6113e56897b0c224bd10df6dec
Koji Ishimoto <koji.ishimoto@gmail.com>
2021-04-03 18:07:27 +0900

    improve audio modal

--- src/components/AudioPlayer/AudioModal.tsx
+++ src/components/AudioPlayer/AudioModal.tsx
@@ -55,14 +55,14 @@ const AudioModal = () => {
 
   return (
     <motion.article
-      className={`flex flex-col fixed inset-0 z-40 h-screen bg-neumorphism mx-4 space-y-4`}
+      className={`flex flex-col justify-around fixed inset-0 z-40 h-screen bg-neumorphism px-4 space-y-2`}
       initial="hidden"
       animate="show"
       exit="hidden"
       variants={variants}
       key="modal"
     >
-      <header className="flex items-center justify-between w-full h-24">
+      <header className="flex items-center justify-between w-full h-16">
         <button
           type="button"
           className="p-2 rounded-full neumorphism-normal active:neumorphism-inset"
@@ -70,25 +70,25 @@ const AudioModal = () => {
           aria-label="オーディオプレイヤーを閉じる"
         >
           <IoChevronDown
-            size={36}
+            size={24}
           />
         </button>
-        <h2 className="text-lg font-medium">{tracks[0].album}</h2>
+        <h2 className="text-lg font-medium line-clamp-1">{tracks[0].album}</h2>
         <div className="w-9 h-9" />
       </header>
-      <section className={`flex flex-col items-center justify-start space-y-4`}>
+      <section className={`flex flex-col items-center`}>
         <GatsbyImage
           loading="eager"
-          className="w-5/6 h-full cursor-none hover:animate-huruhuru"
+          className="w-5/6 h-full sm:w-96 cursor-none"
           image={tracks[0].cover}
           alt={tracks[0].title}
         />
-        <div className="flex flex-col items-start w-full mt-4">
-          <h2 className="font-sans text-3xl font-bold text-black">{tracks[0].title}</h2>
-          <h3 className="font-sans text-lg text-gray-600">{tracks[0].artist}</h3>
-        </div>
       </section>
-      <section>
+      <section className="space-y-2">
+        <div className="flex flex-col items-start w-full">
+          <h2 className="font-sans text-2xl font-bold leading-normal text-black line-clamp-2">{tracks[0].title}</h2>
+          <h3 className="font-sans text-lg text-gray-600 line-clamp-1">{tracks[0].artist}</h3>
+        </div>
         <div className="relative w-full">
           <input
             type="range"
@@ -107,8 +107,8 @@ const AudioModal = () => {
           <span className="text-sm">{displayTime(trackProgress)}</span>
           <span className="text-sm">{displayTime(duration)}</span>
         </div>
+        <AudioControls />
       </section>
-      <AudioControls />
     </motion.article>
   )
 }
